1a88e3e717e4174a0ef6a4171b9f29b262f321c8
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2023-09-20 04:16:20 -0700

    ini sudah menambahkan fortify user login registerdan logout

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,49 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
-
-
 Route::get('/', function () {
-    return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    return view('pages.auth.auth-login', ['type_menu' => '']);
 });
 
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login', ['type_menu' => '']);
-})->name('login');
 
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register', ['type_menu' => '']);
-})->name('register');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('home', function () {
+        return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    })->name('home');
+});
 
 
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot', ['type_menu' => '']);
-})->name('forgot');
 
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password', ['type_menu' => '']);
-})->name('reset-password');
+
+
+// ini saat setting fortify di comment
+
+// Route::get('/', function () {
+//     return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+// });
+
+
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login', ['type_menu' => '']);
+// })->name('login');
+
+
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register', ['type_menu' => '']);
+// })->name('register');
+
+
+
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot', ['type_menu' => '']);
+// })->name('forgot');
+
+
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password', ['type_menu' => '']);
+// })->name('reset-password');
