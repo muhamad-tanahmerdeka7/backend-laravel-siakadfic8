75473b22ccbab0202de00d2f6558fb9439ecad87
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2023-09-20 02:28:05 -0700

    auth pages sudah ready

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
+
+
+
+
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.app.dashboard-siakad', ['type_menu' => '']);
 });
+
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login', ['type_menu' => '']);
+})->name('login');
+
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register', ['type_menu' => '']);
+})->name('register');
+
+
+
+Route::get('/forgot', function () {
+    return view('pages.auth.auth-forgot', ['type_menu' => '']);
+})->name('forgot');
+
+
+Route::get('/reset-password', function () {
+    return view('pages.auth.auth-reset-password', ['type_menu' => '']);
+})->name('reset-password');
